72f02076ae81a3e91b2d10163f5c12438e18557e
Brian Lau <lauwangtatbrian@gmail.com>
2017-05-23 14:53:46 -0700

    added product taxonomy

--- plugins/inhabitent-functionality/lib/functions/taxonomies.php
+++ plugins/inhabitent-functionality/lib/functions/taxonomies.php
@@ -5,4 +5,41 @@
  * @link  http://codex.wordpress.org/Function_Reference/register_taxonomy
  */
 
-// Add your custom taxonomies here...
+// Register Product Type Taxonomy
+function register_product_type_taxonomy() {
+
+	$labels = array(
+		'name'                       => 'Product Types',
+		'singular_name'              => 'Product Type',
+		'menu_name'                  => 'Product Type',
+		'all_items'                  => 'All Product Types',
+		'parent_item'                => 'Parent Product Type',
+		'parent_item_colon'          => 'Parent Product Type:',
+		'new_item_name'              => 'New Product Type',
+		'add_new_item'               => 'Add New Product Type',
+		'edit_item'                  => 'Edit Product Type',
+		'update_item'                => 'Update Product Type',
+		'view_item'                  => 'View Product Type',
+		'separate_items_with_commas' => 'Separate Product Types with commas',
+		'add_or_remove_items'        => 'Add or remove Product Types',
+		'choose_from_most_used'      => 'Choose from the most used',
+		'popular_items'              => 'Popular Product Types',
+		'search_items'               => 'Search Product Types',
+		'not_found'                  => 'Not Found',
+		'no_terms'                   => 'No Product Types',
+		'items_list'                 => 'Product Types list',
+		'items_list_navigation'      => 'Product Types list navigation',
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'product-type', array( 'product' ), $args );
+
+}
+add_action( 'init', 'register_product_type_taxonomy', 0 );
