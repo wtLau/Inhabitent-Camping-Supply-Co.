2aedc00be77f5ae0f54b922654aa3081bb82ec79
Brian Lau <lauwangtatbrian@gmail.com>
2017-06-06 09:21:05 -0700

    added archive-adventure.php and single-adventure.php

---
+++ themes/inhabitent/single-adventure.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The template for displaying all single posts.
+ *
+ * @package Inhabitent_Theme
+ */
+
+get_header(); ?>
+<div class="site-detail">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+
+			<?php the_post_navigation(); ?>
+
+			<?php
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+			?>
+
+		<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+	<?php get_sidebar(); ?>
+</div>
+<?php get_footer(); ?>
