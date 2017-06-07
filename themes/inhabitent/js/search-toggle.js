4d49918ffc54582e25a3c8a906bad0e1d824a328
Brian Lau <lauwangtatbrian@gmail.com>
2017-06-07 15:01:09 -0700

    added dark-nav when scroll below heroimage

--- themes/inhabitent/js/search-toggle.js
+++ themes/inhabitent/js/search-toggle.js
@@ -2,6 +2,7 @@
    $('.search-field').hide();
    $('.fa-search').on('click', function(event) {
       event.preventDefault();
+      event.stopPropagation();
       $('.search-field').toggle('slow');
     })
 
@@ -10,4 +11,23 @@
       $('.search-field').hide('slow');
     }
    });
+
+   $(window).on('scroll', function() {
+      var scrollPosition = $(window).scrollTop();
+      var heroHeight = $('#post-38').height();
+      if(scrollPosition > heroHeight) {
+        $('#site-navigation').show();
+        $('#site-navigation').addClass('active');
+        $('#site-navigation').addClass('main-navigation-dark');
+        $('#site-navigation').removeClass('main-navigation');
+      } else if ( scrollPosition ==0 ) {
+        $('#site-navigation').show();
+        $('#site-navigation').removeClass('active');
+        $('#site-navigation').addClass('main-navigation');
+        $('#site-navigation').removeClass('main-navigation-dark');        
+      } else {
+        $('#site-navigation').hide();
+      }
+
+   })
 })(jQuery);
